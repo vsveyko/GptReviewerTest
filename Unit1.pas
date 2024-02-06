76a1a34eff6328e100ca6bb34d5a41710f44bc7d
Valeriy Sveyko <Valeriy.Sveyko@qda-solutions.com>
2024-02-06 17:16:52 +0100

    initial commit

---
+++ Unit1.pas
@@ -0,0 +1,22 @@
+unit Unit1;
+
+interface
+
+uses // List of unit dependencies goes here...
+  // Interface section goes here
+
+implementation
+
+uses // List of unit dependencies goes here...
+
+// Implementation of class methods, procedures, and functions goes here...
+
+initialization
+
+// Unit initialization code goes here...
+
+finalization
+
+// Unit finalization code goes here...
+
+end.
